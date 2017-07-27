a9eae29d05fc92d0d892d2083458206f036bf75b
Mikki Mesfin <mikki.mesfi@gmail.com>
2017-07-26 20:33:17 -0600

    post is working

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 $(document).ready(function(){
-    // $("button").click(function(){
-        $.get("https://rocky-beach-86199.herokuapp.com/")
+        $.get(baseURL)
         .then(getMakeup)
-// })
 })
-// function getMakeup(data) {
-//   for (var i = 0; i < data.length; i++) {
-//   $(".inventory").append(`<li>${data[i].price}-${data[i].brand}-${data[i].type}</li>`)
-// }
-// }
+var baseURL = 'http://localhost:8080/'
+var otherURL = 'https://rocky-beach-86199.herokuapp.com'
+
 function getMakeup(data) {
   for (var i = 0; i < data.length; i++) {
   $("#inventory").append(`<div class="col-sm-6 col-md-4">
     <div class="thumbnail">
-      <img src="https://cdn2.tgdd.vn/Products/Images/1784/73132/YouCamMakeup-MakeoverStudio-icon.png" alt="makeup">
+      <img src="https://placeimg.com/640/480/people" alt="makeup">
       <div class="caption">
         <h3>${data[i].type}</h3>
         <p> Price:$${data[i].price}</p><p>Brand:${data[i].brand}</p>
@@ -23,3 +19,21 @@ function getMakeup(data) {
   </div>`)
 }
 }
+
+$("button").click(function(event){
+  event.preventDefault()
+  var brandVal = $('#brand').val()
+  var typeVal = $('#type').val()
+  var priceVal = $('#price').val()
+  //make into an object to send as data
+  var resourceObject = {
+    brand: brandVal,
+    type: typeVal,
+    price: priceVal
+  }
+  //posting
+  $.post(baseURL, resourceObject)
+    .then((data) =>
+    data.json()
+  )
+})
